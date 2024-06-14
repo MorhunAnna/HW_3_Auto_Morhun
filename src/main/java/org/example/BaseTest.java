662d22c0484b20a07b44e27e62381d61ec1863aa
Anna <anymorgun13@gmail.com>
2024-06-14 09:40:34 +0300

    d

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -6,17 +6,40 @@ import org.testng.annotations.BeforeMethod;
 
 
 import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public abstract class BaseTest {
+public abstract class BaseTest
+{
 
     public WebDriver webDriver;
 
     @BeforeMethod
-    public void initDriver() {
+    public void initDriver()
+    {
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
-    public void quitDriver() {webDriver.quit();}
+    public void quitDriver()
+    {
+        webDriver.quit();
+    }
+
+ protected String extractDigits(String input)
+    {
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(input);
+
+        StringBuilder digits = new StringBuilder();
+
+        while (matcher.find())
+        {
+            digits.append(matcher.group());
+        }
+
+        return digits.toString();
+    }
 }
+
--- src/main/java/org/example/Prozorro.java
+++ src/main/java/org/example/Prozorro.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import java.time.Duration;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Prozorro extends BaseTest {
     @Test
@@ -19,32 +20,34 @@ public class Prozorro extends BaseTest {
 
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement name =webDriver.findElement(By.xpath("//a[@class='item-title__title']"));
+        WebElement name = webDriver.findElement(By.xpath("//a[@class='item-title__title']"));
         String textName = name.getText();
 
-        WebElement status=webDriver.findElement(By.xpath("//span[@class='getter _setter __v_isRef __v_isReadonly effect _cacheable']"));
+        WebElement status = webDriver.findElement(By.xpath("//span[@class='getter _setter __v_isRef __v_isReadonly effect _cacheable']"));
         String textStatus = status.getText();
 
-        WebElement cost =webDriver.findElement(By.xpath("//p[@class='text-color--green app-price__amount']"));
+        WebElement cost = webDriver.findElement(By.xpath("//p[@class='text-color--green app-price__amount']"));
         String textCost = cost.getText();
+        String costDigits = extractDigits(textCost);
 
-        WebElement enterprise =webDriver.findElement(By.xpath("//div[@class='search-result-card__description']"));
+        WebElement enterprise = webDriver.findElement(By.xpath("//div[@class='search-result-card__description']"));
         String textEnterprise = enterprise.getText();
 
         name.click();
 
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement detailName =webDriver.findElement(By.xpath("//div[@class='tender--head--title col-sm-9']"));
+        WebElement detailName = webDriver.findElement(By.xpath("//div[@class='tender--head--title col-sm-9']"));
         String textDetailName = detailName.getText();
 
-        WebElement detailStatus=webDriver.findElement(By.xpath("//span[@class='marked']"));
+        WebElement detailStatus = webDriver.findElement(By.xpath("//span[@class='marked']"));
         String textDetailStatus = detailStatus.getText();
 
-        WebElement detailCost =webDriver.findElement(By.xpath("//div[@class='green tender--description--cost--number']"));
+        WebElement detailCost = webDriver.findElement(By.xpath("//div[@class='green tender--description--cost--number']"));
         String textDetailCost = detailCost.getText();
+        String costDetailDigits = extractDigits(textDetailCost);
 
-        WebElement detailEnterprise =webDriver.findElement(By.xpath("//td[@class='col-sm-6']"));
+        WebElement detailEnterprise = webDriver.findElement(By.xpath("//td[@class='col-sm-6']"));
 
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", detailEnterprise);
 
@@ -52,7 +55,8 @@ public class Prozorro extends BaseTest {
 
         Assert.assertEquals(textDetailName, textName, "Name does not match");
         Assert.assertEquals(textDetailStatus, textStatus, "Status does not match");
-        Assert.assertEquals(textDetailCost, textCost, "Cost does not match");
-        Assert.assertEquals(textDetailEnterprise, textEnterprise , "The name of the enterprise does not match");
+        Assert.assertEquals(costDetailDigits, costDigits, "Cost does not match");
+        Assert.assertEquals(textDetailEnterprise, textEnterprise, "The name of the enterprise does not match");
     }
 }
+
